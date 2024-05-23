7254b59fb24943454dbb1daf409e7f1ee283998d
Urtix <es.perevalov@mail.ru>
2024-05-24 00:10:01 +0300

    test: Add 100 run test

--- task4/BST/src/test/kotlin/TestsOptimisticBST.kt
+++ task4/BST/src/test/kotlin/TestsOptimisticBST.kt
@@ -7,71 +7,76 @@ import org.junit.jupiter.api.Test
 class TestsOptimisticBST{
     private val nodes: Int = 1000
     private val tree = OptimisticBST<Int>()
-    private var listRandomNodes = (0..nodes).shuffled().take(nodes)
-
     @Test
     fun `one thread test`() = runBlocking {
-        repeat(nodes) {
-            tree.add(listRandomNodes[it])
-        }
+        repeat(100) {
+            val listRandomNodes = (0..nodes).shuffled().take(nodes)
 
-        repeat(nodes) {
-            tree.delete(listRandomNodes[it])
-        }
+            repeat(nodes) {
+                tree.add(listRandomNodes[it])
+            }
 
-        for (key in listRandomNodes) {
-            assertEquals(null, tree.search(key))
+            repeat(nodes) {
+                tree.delete(listRandomNodes[it])
+            }
+
+            for (key in listRandomNodes) {
+                assertEquals(null, tree.search(key))
+            }
         }
 
     }
 
     @Test
     fun `parallel test`() = runBlocking {
-        val headNodes = listRandomNodes.subList(0, listRandomNodes.size / 2)
-        val tailNodes = listRandomNodes.subList(listRandomNodes.size / 2, listRandomNodes.size)
-        coroutineScope {
+        repeat(100) {
+            val listRandomNodes = (0..nodes).shuffled().take(nodes)
+            val headNodes = listRandomNodes.subList(0, listRandomNodes.size / 2)
+            val tailNodes = listRandomNodes.subList(listRandomNodes.size / 2, listRandomNodes.size)
+            coroutineScope {
 
-            launch {
-                delay(50)
-                repeat(nodes/2) {
-                    tree.add(headNodes[it])
+                launch {
+                    delay(50)
+                    repeat(nodes / 2) {
+                        tree.add(headNodes[it])
+                    }
+                }
+                launch {
+                    delay(50)
+                    repeat(nodes / 2) {
+                        tree.add(tailNodes[it])
+                    }
+                }
+
+            }
+
+            val nodesToDelete = listRandomNodes.shuffled(Random).take(nodes / 2)
+            val headNodesToDelete = nodesToDelete.subList(0, nodesToDelete.size / 2)
+            val tailNodesToDelete = nodesToDelete.subList(nodesToDelete.size / 2, nodesToDelete.size)
+
+            coroutineScope {
+                launch {
+                    delay(50)
+                    repeat(nodes / 4) {
+                        tree.delete(headNodesToDelete[it])
+                    }
+                }
+                launch {
+                    delay(50)
+                    repeat(nodes / 4) {
+                        tree.delete(tailNodesToDelete[it])
+                    }
                 }
             }
-            launch {
-                delay(50)
-                repeat(nodes/2) {
-                    tree.add(tailNodes[it])
+
+            for (key in listRandomNodes) {
+                if (key !in nodesToDelete) {
+                    assertEquals(key, tree.search(key))
+                } else {
+                    assertEquals(null, tree.search(key))
                 }
             }
 
         }
-
-        val nodesToDelete = listRandomNodes.shuffled(Random).take(nodes / 2)
-        val headNodesToDelete = nodesToDelete.subList(0, nodesToDelete.size / 2)
-        val tailNodesToDelete = nodesToDelete.subList(nodesToDelete.size / 2, nodesToDelete.size)
-
-        coroutineScope {
-            launch {
-                delay(50)
-                repeat(nodes / 4) {
-                    tree.delete(headNodesToDelete[it])
-                }
-            }
-            launch {
-                delay(50)
-                repeat(nodes / 4) {
-                    tree.delete(tailNodesToDelete[it])
-                }
-            }
-        }
-
-        for (key in listRandomNodes) {
-            if (key !in nodesToDelete) {
-                assertEquals(key, tree.search(key))
-            } else {
-                assertEquals(null, tree.search(key))
-            }
-        }
-
     }
 }
